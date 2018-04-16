0b745733fd4de10203aa5e76875471e09e1bc9f7
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-04-17 00:20:19 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// Define these exported classes
+export class OlderCoaster extends React.Component {
+  // your code here
+  render(){
+    return <div className="oldercoaster">
+      <p>Two grannies having the time of their life!</p>
+      <p>Passengers:</p>
+      <ul>
+        <li>Agnes</li>
+        <li>Muriel</li>
+      </ul>
+    </div>;
+  }
+}
+
+
+
+export class InFrontOfYou extends React.Component {
+  // your code here
+  render(){
+    return <div>
+      <p>You shouldn't look too far.</p>
+      <p>Sometimes, the solution is right in front of you.</p>
+    </div>;
+  }
+}
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
   'Beef shin',
   'Ribeye'
-]
-
-// Define these exported classes
-export class OlderCoaster extends React.Component {
-  // your code here
-}
-
-export class InFrontOfYou extends React.Component {
-  // your code here
-}
+];
 
 export class ButcherShop extends React.Component {
   // your code here
+  render(){
+    return <div class="butcher-shop">
+      <p>Hello! We have the following products for sale today:</p>
+      <ul>
+        {BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))}
+      </ul>
+    </div>;
+  }
 }
 
 
 ReactDOM.render(
   <div>
-    render your components here! Refer back to the previous README if you are lost
+    { React.createElement(OlderCoaster) }
+    {React.createElement(InFrontOfYou)}
+    {React.createElement(ButcherShop)}
   </div>,
   document.getElementById('root')
 )
